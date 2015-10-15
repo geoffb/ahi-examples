617f31e6c471b916e6cc9979929782a37841f6a0
Geoff Blair <geoff@lostdecadegames.com>
2015-10-15 10:08:43 -0700

    Slow it down a bit

--- physics/lib/test.js
+++ physics/lib/test.js
@@ -10,7 +10,7 @@ for (var i = 0; i < 20; ++i) {
 	var player = Entity.fromPrefab("player");
 	player.transform.position.x = Math.round(Math.random() * 640);
 	player.transform.position.y = Math.round(Math.random() * 200);
-	var speed = 0.05 + Math.random() * 0.25;
+	var speed = 0.05 + Math.random() * 0.1;
 	player.rigidBody.velocity.x = Math.random() > 0.5 ? -speed : speed;
 	//player.rigidBody.velocity.y = Math.random() > 0.5 ? -speed : speed;
 	game.addEntity(player);
